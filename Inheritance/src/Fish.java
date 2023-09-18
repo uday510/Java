ddb019417d8f7da675bf87c7e8e79ef3de3eff2a
Uday <admin@udayteja.com>
2023-09-18 12:05:59 +0530

    Add String Class

--- Inheritance/src/Fish.java
+++ Inheritance/src/Fish.java
@@ -13,4 +13,29 @@ public class Fish extends Animal {
         this.gills = gills;
         this.fins = fins;
     }
+
+    private void moveMuscles() {
+        System.out.println("muscles moving ");
+    }
+    private void moveBackFin() {
+        System.out.println("backfin moving ");
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        moveMuscles();
+        if (speed == "fast") {
+            moveBackFin();
+        }
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "gills=" + gills +
+                ", fins=" + fins +
+                "} " + super.toString();
+    }
 }
